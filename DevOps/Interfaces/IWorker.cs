4caa61a1b235b0bafc25d98160ebbc56032d5ad1
Vitalii Palochkin <palochkin99@gmail.com>
2024-11-20 19:35:53 +0200

    Added comments

--- DevOps/Interfaces/IWorker.cs
+++ DevOps/Interfaces/IWorker.cs
@@ -2,13 +2,16 @@ using DevOps.Enums;
 
 namespace DevOps.Interfaces;
 
+/// <summary>
+/// Worker class interpreet classic worker that have own tasks 
+/// </summary>
 public interface IWorker
 {
     string FirstName { get; set; }
     string LastName { get; set; }
     int Age { get; set; }
-    int Telephone { get; set; }
-    int Salary { get; set; }
+    int Telephone { get; set; } //Ukraine telephone number
+    int Salary { get; set; } // Salary in dollars
     List<ITask> Tasks { get; }
 
     void AddTask(ITask task);
